9a5d7705d9e8ac6776d7f5cfa82e291e3732cfc8
MakWaizeon <Makwz3@hotmail.com>
2016-07-10 20:42:47 +0800

    overload counting sort

---
+++ sort/countingSort.h
@@ -0,0 +1,48 @@
+#ifndef __COUNTING_SORT_H__
+#define __COUNTING_SORT_H__
+
+void countingSort(int *arr, int len, int k) {
+    int *tmp = new int[len];
+    for (int i = 0; i < len; i++)
+        tmp[i] = 0;
+
+    int *cnt = new int[k + 1];
+    for (int i = 0; i <= k; i++)
+        cnt[i] = 0;
+
+    for (int i = 0; i < len; i++)
+        ++cnt[arr[i]];
+    for (int i = 1; i <= k; i++)
+        cnt[i] += cnt[i - 1];
+
+    for (int i = len - 1; i >= 0; i--) {
+        tmp[cnt[arr[i]]] = arr[i];
+        --cnt[arr[i]];
+    }
+
+    for (int i = 0; i < len; i++)
+        arr[i] = tmp[i];
+
+    delete[] tmp;
+    delete[] cnt;
+}
+
+void countingSort(int *arrA, int *arrB, int len, int k) {
+    int *cnt = new int[k + 1];
+    for (int i = 0; i <= k; i++)
+        cnt[i] = 0;
+
+    for (int i = 0; i < len; i++)
+        ++cnt[arrA[i]];
+    for (int i = 1; i <= k; i++)
+        cnt[i] += cnt[i - 1];
+
+    for (int i = len - 1; i >= 0; i--) {
+        arrB[cnt[arrA[i]]] = arrA[i];
+        --cnt[arrA[i]];
+    }
+    
+    delete[] cnt;
+}
+
+#endif
