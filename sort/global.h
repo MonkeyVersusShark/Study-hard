8360482d8932cac5c9bf8d927da8ea738a40c530
MakWaizeon <Makwz3@hotmail.com>
2016-07-11 12:04:13 +0800

    improve swap

--- sort/global.h
+++ sort/global.h
@@ -1,10 +1,12 @@
 #ifndef __GLOBAL_H__
 #define __GLOBAL_H__
 
-void swap(int *n1, int *n2) {
-    int temp = *n1;
-    *n1 = *n2;
-    *n2 = temp; 
+inline void swap(int *n1, int *n2) {
+    if (n1 != n2) {
+        int temp = *n1;
+        *n1 = *n2;
+        *n2 = temp; 
+    }
  }
 
 #endif
