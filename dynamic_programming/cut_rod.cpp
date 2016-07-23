00f1dbd628cac8e9bcd7ea9d9b205595709339ce
MakWaizeon <Makwz3@hotmail.com>
2016-07-23 22:31:55 +0800

    add the function which is to get solution

--- dynamic_programming/cut_rod.cpp
+++ dynamic_programming/cut_rod.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <algorithm> // max()
+#include <algorithm>
+#include <utility>
+#include <vector>
 using namespace std;
 
 int p[10] = { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
@@ -57,11 +59,42 @@ int bottom_up_cut_rod(int *p, int n) {
     return result;
 }
 
+// Extended bottom up
+// Get the solution
+int *extended_bottom_up_cut_rod(int *p, int n) {
+    int *r = new int[n + 1];
+    int *s = new int[n + 1];
+    r[0] = 0;
+    for (int j = 1; j <= n; j++) {
+        int q = -1;
+        for (int i = 0; i < j; i++) {
+            if (q < p[i] + r[j - 1 - i]) {
+                q = p[i] + r[j - 1 - i];
+                s[j] = i + 1;
+            }
+        r[j] = q;
+        }
+    }
+    return s;
+}
+
+void print_cut_rod_solution(int *p, int n) {
+    int *result = extended_bottom_up_cut_rod(p, n);
+    while (n > 0) {
+        cout << result[n] << " ";
+        n = n - result[n];
+    }
+    cout << endl;
+    delete[] result;
+}
+
 int main() {
     for (int i = 1; i <= 10; i++) {
         cout << "TEST 1\t" << cut_rod(p, i) << endl;
         cout << "TEST 2\t" << memoized_cut_rod(p, i) << endl;
         cout << "TEST 3\t" << bottom_up_cut_rod(p, i) << endl;
     }
+    for (int i = 1; i <= 10; i++)
+        print_cut_rod_solution(p, i);
     return 0;
 }
